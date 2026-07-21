fea1ed0b1b7208e8b4f14a4bbdaa2dba3d77bb16
srujana <srujanashetty2016@gmail.com>
2026-07-21 14:41:58 +0530

    first commit

---
+++ ex2.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "a27df1dc-c993-4535-af23-834222f51426",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
