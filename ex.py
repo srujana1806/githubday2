f9183f98cdab896866d09dff868940f49c5664c3
srujana <srujanashetty2016@gmail.com>
2026-07-21 15:32:16 +0530

    modified ex.py

--- ex.py
+++ ex.py
@@ -1,1 +1,17 @@
-print('6)
+a=7,'p',9
+print(type(a))
+
+# try to find out vowel from sentence
+
+a='hello,how are you?'
+str='aeiouAEIOU'
+
+for i in a:
+    if i in str:
+        print(i)  
+
+
+sentence='hello,how are you?'
+a=[i for i in sentence if i in 'aeiouAEIOU']
+a
+             
